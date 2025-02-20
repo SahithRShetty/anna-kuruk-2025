552f9be5e2711f961a3f84c6cd14b2898bce9e51
Sahith R Shetty <shetty@Sahiths-MacBook-Pro.local>
2025-02-20 11:56:36 +0530

    Multimodel code

--- multimodel-llama-3.2/utils.py
+++ multimodel-llama-3.2/utils.py
@@ -26,7 +26,7 @@ def llama32(messages, model_size=11):
   headers = {
     "Accept": "application/json",
     "Content-Type": "application/json",
-    "Authorization": "Bearer <bearer-token>"
+    "Authorization": "Bearer tgp_v1_QmX79vqz1k5Mij5bdyy-FsWeOS5UaRLNaZMCIbGZY4w"
   }
   res = json.loads(requests.request("POST", url, headers=headers, data=json.dumps(payload)).content)
 
